b5345c03458c17adb88f66d6717e7f7609bdb8fc
indu <indhukirthika@gmail.com>
2016-03-04 19:47:46 +0530

    adding pages

---
+++ app/dist/js/app.js
@@ -0,0 +1,78 @@
+
+
+$('#count-example').jQuerySimpleCounter({
+
+  // start number
+  start:  0,
+
+  // end number
+  end:    1580,
+
+  // easing effect
+  easing: 'swing',
+
+  // duration time in ms
+  duration: 5000,
+
+  // callback function
+  complete: ''
+
+});
+
+
+$('#count-example1').jQuerySimpleCounter({
+
+  // start number
+  start:  0,
+
+  // end number
+  end:    1380,
+
+  // easing effect
+  easing: 'swing',
+
+  // duration time in ms
+  duration: 5000,
+
+  // callback function
+  complete: ''
+
+});
+
+$('#count-example2').jQuerySimpleCounter({
+
+  // start number
+  start:  0,
+
+  // end number
+  end:    1080,
+
+  // easing effect
+  easing: 'swing',
+
+  // duration time in ms
+  duration: 5000,
+
+  // callback function
+  complete: ''
+
+});
+
+$('#count-example3').jQuerySimpleCounter({
+
+  // start number
+  start:  0,
+
+  // end number
+  end:    1900,
+
+  // easing effect
+  easing: 'swing',
+
+  // duration time in ms
+  duration: 5000,
+
+  // callback function
+  complete: ''
+
+});
